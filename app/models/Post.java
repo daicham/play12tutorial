da57f3faf6fb3c6719f43a48d54f34f71582a188
daicham <daicham@gmail.com>
2012-07-01 14:19:10 +0900

    モデル:tag を追加。テストも追加

--- app/models/Post.java
+++ app/models/Post.java
@@ -19,8 +19,12 @@ public class Post extends Model {
   @OneToMany(mappedBy="post",cascade=CascadeType.ALL)
   public List<Comment> comments;
 
+  @ManyToMany(cascade=CascadeType.PERSIST)
+  public Set<Tag> tags;
+
   public Post(User author, String title, String content) {
     this.comments = new ArrayList<Comment>();
+    this.tags = new TreeSet<Tag>();
     this.author = author;
     this.title = title;
     this.content = content;
@@ -40,6 +44,17 @@ public class Post extends Model {
 
   public Post next() {
     return Post.find("postedAt > ? order by postedAt asc", postedAt).first();
+  }
+
+  public Post tagItWith(String name) {
+    tags.add(Tag.findOrCreateByName(name));
+    return this;
+  }
+
+  public static List<Post> findTaggedWith(String tag) {
+    return Post.find(
+        "select distinct p from Post p join p.tags as t where t.name = ?", tag
+    ).fetch();
   }
 }
 
